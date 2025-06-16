41503cbac9465fdee7f2cba1ba86e4170728be39
Kumbhakarn <178929455+Kumbhakarn@users.noreply.github.com>
2025-06-16 10:32:05 +0530

    HuggingFace Embedding Models to convert docs into vectors

--- 03.EmbeddedModels/1_embedding_hf.py
+++ 03.EmbeddedModels/1_embedding_hf.py
@@ -7,9 +7,12 @@ os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
 
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
+# convert this document in embeddings 
 documents = ['Delhi is the capital of India',
              'Kolkata is the capital of West Bengal',
              'Paris is the capital of France'
              ]
+
+# Store it in variable 
 vector = embedding.embed_documents(documents) # Multiple list documents 
 print(str(vector))
--- 03.EmbeddedModels/2_embedding_hf_.py
+++ 03.EmbeddedModels/2_embedding_hf_.py
@@ -1,7 +1,9 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
+# Importing Model using HuggingFace
 embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 text = 'Delhi is the capital of India'
 
+# Store it in variable
 vector = embedding.embed_query(text)
 print(str(vector))
